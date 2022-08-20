fe9fc5d13eed3856933a0b1690048d5d3f40c57a
michaeljoslyn <44362644+michaeljoslyn@users.noreply.github.com>
2022-08-20 11:58:42 +0800

    polishing

--- header.php
+++ header.php
@@ -20,10 +20,6 @@ include 'processes/conn.php';
     <nav class="navbar navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand">Guns</a>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
         </div>
     </nav>
     <div class="container">
